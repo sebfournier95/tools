577d45266a56b4df891d18ac90cecf3f781a9d72
antoinefa <fabien.antoine@m4x.org>
2020-03-18 09:39:22 +0100

    add Dockerfile for swift container

---
+++ Dockerfile.swift
@@ -0,0 +1,23 @@
+FROM alpine:latest
+ARG http_proxy
+ARG https_proxy
+ARG no_proxy
+ARG app_path
+ARG app_name
+ARG app_ver
+ENV APP ${APP}
+ENV APP_VERSION ${app_ver}
+
+VOLUME /$app_path/data
+
+WORKDIR /$app_path
+
+RUN apk add --no-cache \
+    python3 \
+    gcc python3-dev musl-dev linux-headers
+RUN pip3 install --upgrade pip
+RUN pip3 install --user setuptools python-swiftclient python-keystoneclient
+RUN mv /root/.local/bin/* /usr/local/bin
+RUN apk -v --purge del gcc python3-dev musl-dev linux-headers
+
+ENTRYPOINT ["swift"]
